b51dccdff33c3d258ad57483e193f988d38a446f
Feroz Ghauri <mferozghauri@gmail.com>
2020-05-12 02:30:35 +0500

    finalized js

--- background.js
+++ background.js
@@ -1,42 +1,14 @@
   
 window.data = {}
-
-
-// var req = new XMLHttpRequest();
-// req.addEventListener('readystatechange', function (evt) {
-//   if (req.readyState === 4) {
-//     if (req.status === 200) {
-//       alert('Saved !');
-//     } else {
-//       alert("ERROR: status " + req.status);
-//     }
-//   }
-// });
-// req.open('POST', 'http://localhost:8080/chromext/insertdb.php', true);
-// req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-// req.send('feroz');
-
-
-
 setInterval(() => {
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     window.data[request.time] = request.value
-
-    // var req = new XMLHttpRequest();
-
-    // req.open('POST', 'http://localhost:8080/chromext/insertdb.php', true);
-    // req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    // req.send("url="+encodeURIComponent(request.time)+"&content="+encodeURIComponent(request.value));
-
-
  })
 }, 3000);
 
-
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-   sendCurrentUrl(tab.url);
-});
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//    sendCurrentUrl(tab.url);
+// });
 
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.create({url: 'popup.html'})
--- insertdb.php
+++ insertdb.php
@@ -1,11 +1,11 @@
 <?php
-if (isSet($_POST['url'])) {
-    $con = mysqli_connect('localhost', 'root', '', 'chromeext');
-    ...
-    $stmt = mysqli_prepare($con, 'INSERT INTO maintable (ID,Date,Time,VALUE) VALUES('1','07052020',$_POST['url'],'0.6241')');
-    //mysqli_stmt_bind_param($stmt, 's', $_POST['url']);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-    mysqli_close($con);
+$connect = mysqli_connect("localhost", "root", "", "chromeext");
+$url = $_POST['url'];
+$content = mysqli_real_escape_string($connect,$_POST['content']);
+$query = "INSERT INTO maintable (ID,Date,Time,VALUE) VALUES ('',CURDATE(),'$url','$content')";
+$data = mysqli_query ($connect, $query); 
+if($data) 
+{ 
+    echo "Database insert complete";
 }
 ?>
